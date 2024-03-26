4c8db32a10cd1e3e1fae96f39e1062acf9a394c6
eyern <eyern254@gmail.com>
2024-03-26 15:51:02 +0300

    js files

--- js/app.js
+++ js/app.js
@@ -740,3 +740,12 @@ $(function () {
     });
 
 })(jQuery, window, document);
+
+var close_button = document.querySelector(".close-button");
+var social_buttons = document.querySelectorAll(".social");
+
+close_button.addEventListener('click',()=>{
+   social_buttons.forEach(function(buttons){
+       buttons.classList.toggle('hide');
+   });
+});
